8c0208ed0f4ac6c5e4715dae4b9a2e7a8c05621f
shzhw <shzhw407@163.com>
2018-07-03 15:56:08 +0800

    添加app返回事件

--- src/main.js
+++ src/main.js
@@ -20,9 +20,39 @@ Vue.use(VueLazyLoad, {
 fastclick.attach(document.body);
 
 /* eslint-disable no-new */
-new Vue({
+let vue = new Vue({
   el: '#app',
   router,
   store,
   render: h => h(App)
+});
+
+let curtime = null;
+document.addEventListener('plusready', function() {
+  const routeList = ['/recommend', '/singer', '/rank', '/search'];
+  /* eslint-disable no-undef  */
+  plus.key.addEventListener(
+    'backbutton',
+    function() {
+      // 先判斷播放器浮層
+      if (vue.$store.state.fullScreen) {
+        vue.$store.commit('SET_FULL_SCREEN', false);
+        return;
+      }
+      // 先判斷路徑
+      const curPath = vue.$route.path;
+      if (routeList.indexOf(curPath) === -1) {
+        vue.$router.back();
+        return;
+      }
+      // 再判斷時間
+      if (!curtime || new Date() - curtime > 3000) {
+        curtime = new Date();
+        plus.nativeUI.toast('再按一次退出程序！');
+      } else {
+        plus.runtime.quit();
+      }
+    },
+    false
+  );
 });
