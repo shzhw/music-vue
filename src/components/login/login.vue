fc895083c3efcf6854671476d6a117a5da048a04
shzhw <shzhw407@163.com>
2018-07-09 14:46:24 +0800

    master登陸注冊頁面添加scroll

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -2,27 +2,30 @@
    <slide-layer ref="slideLayer">
     <div class="login_box">
       <router-link class="register_btn" tag="a" to="/register">注册</router-link>
-      <form action="/api/login_post" @submit.prevent="login">
-        <ul class="login_inner">
-          <li class="username">
-            <input type="text" placeholder="请输入用户名" autocomplete="off" v-model="username">
-            <div class="text_box">
-              <span class="text" v-if="errorMessage">{{errorMessage.text}}</span>
-            </div>
-          <li class="password">
-            <input type="password" class="pswd" placeholder="请输入密码" autocomplete="off" v-model="userpwd">
-          </li>
-          <li class="login_btn_box">
-            <button type="submit" class="login_btn">登陆</button>
-          </li>       
-        </ul>
-      </form>
+      <scroll class="scroll_box">
+        <form action="/api/login_post" @submit.prevent="login" class="scroll_cnt">
+          <ul class="login_inner">
+            <li class="username">
+              <input type="text" placeholder="请输入用户名" autocomplete="off" v-model="username">
+              <div class="text_box">
+                <span class="text" v-if="errorMessage">{{errorMessage.text}}</span>
+              </div>
+            <li class="password">
+              <input type="password" class="pswd" placeholder="请输入密码" autocomplete="off" v-model="userpwd">
+            </li>
+            <li class="login_btn_box">
+              <button type="submit" class="login_btn">登陆</button>
+            </li>       
+          </ul>
+        </form>
+      </scroll>
     </div>
   </slide-layer>
 </template>
 
 <script type="text/ecmascript-6">
 import SlideLayer from '@/base/slide-layer/slide-layer';
+import Scroll from '@/base/scroll/scroll';
 import Users from '@/api/Users';
 import { mapMutations } from 'vuex';
 
@@ -74,7 +77,8 @@ export default {
     })
   },
   components: {
-    SlideLayer
+    SlideLayer,
+    Scroll
   }
 };
 </script>
@@ -87,8 +91,12 @@ export default {
   width: 100%
   height: 100%
   bg-image('login_bg')
-  bakcground-repeat: no-repeat
+  background-repeat: no-repeat
   background-size: 100%
+  background-color: #320808
+  &:before
+    content: ''
+    display: table
   .register_btn
     color: $color-theme
     position: absolute
@@ -96,39 +104,46 @@ export default {
     top: 0
     height: 42px
     line-height: 42px
-  .login_inner
-    width: 80%
-    height: 300px
-    position: absolute
-    top: 50%
-    left: 50%
-    transform: translate(-50%, -50%)
-    >li
-      width: 100%
-      input
-        display: block
-        width: 100%
-        line-height: 35px
-        height: 35px
-        padding: 0 10px
-        box-sizing: border-box
-        border: none
-      .text_box
-        line-height: 30px
-        color: $color-theme
-        font-style: italic
-        font-size: 14px
-        margin-bottom: 10px
-      .login_btn
-        margin-top: 30px
-        width: 100%
-        height: 100%
-        line-height: 35px
-        text-align: center
-        border: none
-        border-radius: 5px
-        font-weight: bold
-        letter-spacing: 7px
-        color: #fff
-        background-color: $color-theme
+  .scroll_box
+    margin-top: 42px
+    overflow: hidden
+    height: 100%
+    .scroll_cnt
+      position: relative
+      min-height: 200px
+      height: 100%
+      .login_inner
+        width: 80%
+        position: absolute
+        top: 45%
+        left: 50%
+        transform: translate(-50%, -50%)
+        >li
+          width: 100%
+          input
+            display: block
+            width: 100%
+            line-height: 35px
+            height: 35px
+            padding: 0 10px
+            box-sizing: border-box
+            border: none
+          .text_box
+            line-height: 30px
+            color: $color-theme
+            font-style: italic
+            font-size: 14px
+            margin-bottom: 10px
+          .login_btn
+            margin-top: 30px
+            width: 100%
+            height: 100%
+            line-height: 35px
+            text-align: center
+            border: none
+            border-radius: 5px
+            font-weight: bold
+            letter-spacing: 7px
+            color: #fff
+            background-color: $color-theme
 </style>
--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -1,47 +1,50 @@
 <template>
   <slide-layer ref="slideLayer">
     <div class="register_box">
-      <form action="/api/register_post" @submit.prevent="register">
-        <ul class="register_inner">
-          <li class="input_box">
-            <input type="text" placeholder="昵称" autocomplete="off" v-model="v_nickname">
-            <span class="text_box" v-if="nickname.message">{{nickname.message}}</span>
-          </li>
-          <li class="input_box">
-            <input type="text" placeholder="请输入用户名" autocomplete="off" v-model="v_username">
-            <span class="text_box" v-if="username.message">{{username.message}}</span>
-          </li>
-          <li class="input_box">
-            <input type="password" placeholder="密码" autocomplete="off" v-model="v_pwd">
-            <span class="text_box" v-if="pwd.message">{{pwd.message}}</span>
-          </li>
-          <li class="input_box">
-            <input type="password" placeholder="确认密码" autocomplete="off" v-model="v_rppwd">
-            <span class="text_box" v-if="rppwd.message">{{rppwd.message}}</span>
-          </li>
-          <li class="not_cm_li v_code_box">
-            <div class="wrap">
-              <input type="text" v-model="v_ucode">
-              <!-- <div class="v_code_img">abcd</div> -->
-              <canvas class="v_code_img" ref="myCanvas" width="70px" height="35px" @click="createCode"></canvas>
-            </div>
-            <span class="text_box" v-if="ucode.message">{{ucode.message}}</span>
-          </li>
-          <li class="agreement_box">
-              <input type="checkbox" class="agreement_ck" id="agreement_ck" checked>
-              <label class="agreement" for="agreement_ck">阅读并接受<a href="javascript:;">《用户协议》</a>及<a href="javascript:;">《隐私权保护声明》</a></label>
-          </li>
-          <li>
-            <button class="register_btn" type="submit" :disabled="!valid">提交</button>
-          </li>
-        </ul>
-      </form>
+      <scroll class="scroll_box">
+        <form action="/api/register_post" @submit.prevent="register" class="scroll_cnt">
+          <ul class="register_inner">
+            <li class="input_box">
+              <input type="text" placeholder="昵称" autocomplete="off" v-model="v_nickname">
+              <span class="text_box" v-if="nickname.message">{{nickname.message}}</span>
+            </li>
+            <li class="input_box">
+              <input type="text" placeholder="请输入用户名" autocomplete="off" v-model="v_username">
+              <span class="text_box" v-if="username.message">{{username.message}}</span>
+            </li>
+            <li class="input_box">
+              <input type="password" placeholder="密码" autocomplete="off" v-model="v_pwd">
+              <span class="text_box" v-if="pwd.message">{{pwd.message}}</span>
+            </li>
+            <li class="input_box">
+              <input type="password" placeholder="确认密码" autocomplete="off" v-model="v_rppwd">
+              <span class="text_box" v-if="rppwd.message">{{rppwd.message}}</span>
+            </li>
+            <li class="not_cm_li v_code_box">
+              <div class="wrap">
+                <input type="text" v-model="v_ucode">
+                <!-- <div class="v_code_img">abcd</div> -->
+                <canvas class="v_code_img" ref="myCanvas" width="70px" height="35px" @click="createCode"></canvas>
+              </div>
+              <span class="text_box" v-if="ucode.message">{{ucode.message}}</span>
+            </li>
+            <li class="agreement_box">
+                <input type="checkbox" class="agreement_ck" id="agreement_ck" checked>
+                <label class="agreement" for="agreement_ck">阅读并接受<a href="javascript:;">《用户协议》</a>及<a href="javascript:;">《隐私权保护声明》</a></label>
+            </li>
+            <li>
+              <button class="register_btn" type="submit" :disabled="!valid">提交</button>
+            </li>
+          </ul>
+        </form>
+      </scroll>
     </div>
   </slide-layer>
 </template>
 
 <script>
 import SlideLayer from '@/base/slide-layer/slide-layer';
+import Scroll from '@/base/scroll/scroll';
 import Users from '@/api/Users';
 import { mapMutations } from 'vuex';
 
@@ -269,7 +272,8 @@ export default {
     })
   },
   components: {
-    SlideLayer
+    SlideLayer,
+    Scroll
   }
 };
 </script>
@@ -282,71 +286,83 @@ export default {
   width: 100%
   height: 100%
   bg-image('login_bg')
-  bakcground-repeat: no-repeat
+  background-repeat: no-repeat
   background-size: 100%
-  .register_inner
-    width: 80%
-    position: absolute
-    top: 50%
-    left: 50%
-    transform: translate(-50%, -50%)
-    >li
-      width: 100%
-      margin-bottom: 30px
-      >input
-        line-height: 35px
-        height: 35px
-        padding: 0 10px
-        box-sizing: border-box
-        border: none
-      &.input_box
-        >input
-          display: block
+  background-color: #320808
+  &:before
+    content: ''
+    display: table
+  .scroll_box
+    margin-top: 42px
+    overflow: hidden
+    height: 100%
+    .scroll_cnt
+      position: relative
+      min-height: 450px
+      height: 100%
+      .register_inner
+        width: 80%
+        position: absolute
+        top: 45%
+        left: 50%
+        transform: translate(-50%, -50%)
+        >li
           width: 100%
-      .text_box
-        float: left
-        line-height: 30px
-        color: $color-theme
-        font-style: italic
-        font-size: 14px
-      &.v_code_box
-        .wrap
-          display: flex
-          input
-            flex: 0 0 150px
-            width: 150px
+          margin-bottom: 30px
+          >input
+            line-height: 35px
+            height: 35px
+            padding: 0 10px
+            box-sizing: border-box
+            border: none
+          &.input_box
+            >input
+              display: block
+              width: 100%
+          .text_box
+            float: left
+            line-height: 30px
+            color: $color-theme
+            font-style: italic
+            font-size: 14px
+          &.v_code_box
+            .wrap
+              display: flex
+              input
+                flex: 0 0 150px
+                width: 150px
+                text-align: center
+                font-size: 18px
+              .v_code_img
+                flex: 1
+                margin-left: 30px
+                background-color: #ccc
+          &.agreement_box
+            display: flex
+            height: 18px
+            line-height: 18px
+            margin-bottom: 0
+            input
+              flex: 0 0 15px
+              height: 100%
+            .agreement
+              flex: 1
+          .agreement
+            font-size: 12px
+            a
+              color: $color-theme
+          .register_btn
+            margin-top: 12px
+            width: 100%
+            height: 100%
+            line-height: 35px
             text-align: center
-            font-size: 18px
-          .v_code_img
-            flex: 1
-            margin-left: 30px
-            background-color: #ccc
-      &.agreement_box
-        display: flex
-        height: 18px
-        line-height: 18px
-        margin-bottom: 0
-        input
-          flex: 0 0 15px
-          height: 100%
-        .agreement
-          flex: 1
-      .agreement
-        font-size: 12px
-        a
-          color: $color-theme
-      .register_btn
-        margin-top: 12px
-        width: 100%
-        height: 100%
-        line-height: 35px
-        text-align: center
-        border: none
-        border-radius: 5px
-        font-weight: bold
-        letter-spacing: 7px
-        color: #fff
-        background-color: $color-theme
-        &:disabled
-          background-color: #aaa
+            border: none
+            border-radius: 5px
+            font-weight: bold
+            letter-spacing: 7px
+            color: #fff
+            background-color: $color-theme
+            &:disabled
+              background-color: #aaa
 </style>
